5e66852174b564fa45a5ee545f1bbdd971d9a4a4
sahil-kikani <sahil.kikani@yudiz.com>
2024-01-23 11:20:50 +0530

    client id check

--- shared/Components/googleButton/index.tsx
+++ shared/Components/googleButton/index.tsx
@@ -11,15 +11,15 @@ export default function GoogleSignIn({ ...props }) {
   function setupGoogleLogin() {
     const { google } = window
     if (google) {
-      console.log(google.accounts)
-      // google.accounts.id.initialize({
-      //   client_id: '454435860226-vlva6pfrt2k7mm19m80c5q2h7fps1dof.apps.googleusercontent.com',
-      //   callback: async (response) => console.log(response)
-      // })
-      // google.accounts.id.renderButton(document.getElementById('google-sign-btn'), {
-      //   shape: 'rectangular',
-      //   theme: 'filled_blue'
-      // })
+      console.log(google.accounts, )
+      google.accounts.id.initialize({
+        client_id: '129082485861-54j76vgc3ltheumjqu36m0cp4rvajoha.apps.googleusercontent.com',
+        callback: async (response) => console.log(response)
+      })
+      google.accounts.id.renderButton(document.getElementById('google-sign-btn'), {
+        shape: 'rectangular',
+        theme: 'filled_blue'
+      })
     }
   }
 
@@ -34,29 +34,11 @@ export default function GoogleSignIn({ ...props }) {
       // script.setAttribute('strategy', 'lazyOnload')
       document.head.appendChild(script)
       script.onload = () => {
-        // setupGoogleLogin()
+        setupGoogleLogin()
       }
     }
   }, [])
-
   return (
-    <>
-    <div id="g_id_onload"
-     data-client_id="129082485861-54j76vgc3ltheumjqu36m0cp4rvajoha.apps.googleusercontent.com"
-     data-context="signin"
-     data-ux_mode="popup"
-     data-callback="(response) => console.log(response)"
-     data-auto_prompt="false">
-</div>
-
-<div className="g_id_signin"
-     data-type="standard"
-     data-shape="rectangular"
-     data-theme="outline"
-     data-text="signin_with"
-     data-size="large"
-     data-logo_alignment="left">
-</div>
-    </>
+    <div id='google-sign-btn' {...props}></div>
   )
 }
